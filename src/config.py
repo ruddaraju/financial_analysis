12b528950c12fc75133104ee233c46debd33a2d7
ruddaraj <ruddaraj@purdue.edu>
2025-08-30 14:42:08 -0400

    Config

---
+++ src/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+AV_API_KEY = os.getenv('ALPHAVANTAGE_API_KEY')
+AV_BASE = os.getenv('ALPHAVANTAGE_BASE', 'https://www.alphavantage.co/query')
+
+UNIVERSE = ['META','AAPL','NVDA','GOOG','TSLA','JPM','AMZN','GE','PLTR']
