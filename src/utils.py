cac5ca2d4236015b17ba19ddf69623c26b2fed37
ruddaraj <ruddaraj@purdue.edu>
2025-08-31 14:23:18 -0400

    Push Utils

---
+++ src/utils.py
@@ -0,0 +1,16 @@
+import time
+import json
+from pathlib import Path
+
+RAW_DIR = Path(__file__).resolve().parent[1] / 'data_raw'
+RAW_DIR.mkdir(exist_ok = True)
+
+
+def save_raw(obj, name: str):
+    path = RAW_DIR / f'{name}.json'
+    with open(path, 'w') as f:
+        json.dump(obj, f, indent=2)
+    return str(path)
+
+def rate_limit_sleep(seconds = 12):
+    time.sleep(seconds)
